02e5fc54b8955df26a07d811c86488988931c667
etlers <etlersadm@gmail.com>
2021-03-11 17:24:52 +0900

    first upload

---
+++ nodejs/app.js
@@ -0,0 +1,10 @@
+console.log("Hello etlers!!")
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type' : 'text/plain' });
+  res.end('Hello World');
+});
+
+server.listen(8000);
